f000f9725b0abaf7efb14fb756aa89e8ec50feae
twishavyas <vyas.tw@husky.neu.edu>
2017-11-18 20:01:59 -0500

    a

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Login</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+
+
+
+
+
+
+                            <br />
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <a href="{{url('/redirect')}}" class="btn btn-primary">Login with Facebook</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
